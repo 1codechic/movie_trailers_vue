cc0f12eb6a73cecc0b528d9e7b3109fe3ee50de8
1codechic <1codechic@devchics.com>
2019-04-08 18:05:50 -0400

    added index, and createMovie function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Movies</h2>
+    <div>
+      <p>Name: <input type="text" v-model="newMovie.name"></p>
+      <p>Duration: <input type="text" v-model="newMovie.duration"></p>
+      <p>Photo URL: <input type="text" v-model="newMovie.photo"></p>
+      <button v-on:click="createMovie()">Create Movie</button>
+      <hr>
+    </div>
+    <div v-for="movie in movies">
+      <img v-bind:src="movie.photo">
+      <p>Name: {{ movie.name }}</p>
+      <p>Duration: {{ movie.duration }}</p>
+      <hr>
+    </div>
+
   </div>
 </template>
 
+<style>
+  img {
+    width: 250px;
+  }
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      movies: [],
+      newMovie: {
+        name: "",
+        duration: "",
+        photo: ""
+      }
+    };
+  },
+  created: function() {
+    axios.get("/api/movies").then(response => {
+      this.movies = response.data;
+    });
+  },
+  methods: {
+    createMovie: function() {
+      var params = {
+        name: this.newMovie.name,
+        duration: this.newMovie.duration,
+        photo: this.newMovie.photo
+      }
+      axios.post("/api/movies", params).then(response => {
+        console.log(response);
+        this.movies.push(response.data);
+      });
+    }
   }
-}
+};
 </script>
+
